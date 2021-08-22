1463cd800c5c360cd75563a59086a21a00e48c4a
Bryson Havics <brysonhavics@gmail.com>
2021-08-21 20:47:43 -0400

    Added CRUD

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KomodoCafe_Repository;
 
-namespace GoldBadgeChallenges
+namespace KomodoCafe_Console;
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            MenuRepository menu = new MenuRepository();
         }
     }
 }
